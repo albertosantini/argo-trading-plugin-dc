82e639ec3fd4fe36693303ff319e0d1cae4f82c0
icebox <albertosantini@gmail.com>
2019-08-08 09:15:21 +0200

    chore(linter): eslint 6.1.0 and migrate to ES2015

--- test/aroon-test.js
+++ test/aroon-test.js
@@ -1,18 +1,16 @@
 "use strict";
 
-var test = require("tape");
-var dc = require("../lib/signals/dc");
+const test = require("tape");
+const dc = require("../lib/signals/dc");
 
-test("aroonUp", function (t) {
-    var uptrends = [
-            {pext: 1.29840, pos: 1.29990},
-            {pext: 1.30038, pos: 1.30175},
-            {pext: 1.29940, pos: 1.30072},
-            {pext: 1.30065, pos: 1.30200}
-        ],
-        aroon;
-
-    aroon = dc.aroonUp(uptrends, 3);
+test("aroonUp", t => {
+    const uptrends = [
+        { pext: 1.29840, pos: 1.29990 },
+        { pext: 1.30038, pos: 1.30175 },
+        { pext: 1.29940, pos: 1.30072 },
+        { pext: 1.30065, pos: 1.30200 }
+    ];
+    const aroon = dc.aroonUp(uptrends, 3);
 
     t.plan(2);
 
@@ -20,17 +18,16 @@ test("aroonUp", function (t) {
     t.equal(aroon.downs[0], 0, "aroon uptrends downs");
 });
 
-test("aroonDown", function (t) {
-    var downtrends = [
-            {pext: 1.30245, pos: 1.30090},
-            {pext: 1.30224, pos: 1.30081},
-            {pext: 1.30238, pos: 1.30105},
-            {pext: 1.30430, pos: 1.30293}
-        ],
-        aroon;
+test("aroonDown", t => {
+    const downtrends = [
+        { pext: 1.30245, pos: 1.30090 },
+        { pext: 1.30224, pos: 1.30081 },
+        { pext: 1.30238, pos: 1.30105 },
+        { pext: 1.30430, pos: 1.30293 }
+    ];
+    const aroon = dc.aroonDown(downtrends, 3);
 
-    aroon = dc.aroonDown(downtrends, 3);
-    console.log(aroon);
+    console.log(aroon); // eslint-disable-line
 
     t.plan(2);
 
--- test/dc-test.js
+++ test/dc-test.js
@@ -1,25 +1,25 @@
 "use strict";
 
-var test = require("tape");
-var series = require("./ftse100-series").ftse100;
-var strategy = require("../lib/signals/dc");
+const test = require("tape");
+const series = require("./ftse100-series").ftse100;
+const strategy = require("../lib/signals/dc");
 
-var expected = [
-    {index: 60, price: 5586.61, trend: -1, hi: 5825.01, lo: 5586.61, up: 0, down: 0, ext: 5825.01, change: -1, osv: 0},
-    {index: 92, price: 5217.82, trend: 1, hi: 5217.82, lo: 5028.15, up: 0, down: 0, ext: 5028.15, change: 1, osv: 0},
-    {index: 192, price: 5748.97, trend: 1, hi: 5757.86, lo: 5109.4, up: 0, down: 0, ext: 5109.4, change: 4, osv: 3},
-    {index: 292, price: 5904.49, trend: 1, hi: 5904.49, lo: 5598.23, up: 0, down: 0, ext: 5598.23, change: 1, osv: 0},
-    {index: 392, price: 5095.3, trend: -1, hi: 5357.63, lo: 5040.76, up: 0, down: 0, ext: 5357.63, change: -1, osv: 0},
-    {index: 492, price: 5636.64, trend: 1, hi: 5699.91, lo: 5364.99, up: 0, down: 0, ext: 5364.99, change: 1, osv: 0},
-    {index: 592, price: 5435.08, trend: 1, hi: 5447.79, lo: 5260.19, up: 0, down: 0, ext: 5260.19, change: 1, osv: 0},
-    {index: 692, price: 5795.1, trend: 1, hi: 5917.05, lo: 5657.86, up: 0, down: 0, ext: 5657.86, change: 0, osv: 0},
-    {index: 792, price: 6388.55, trend: 1, hi: 6529.41, lo: 5605.59, up: 0, down: 0, ext: 5605.59, change: 4, osv: 3},
-    {index: 892, price: 6587.43, trend: 1, hi: 6681.98, lo: 6029.1, up: 0, down: 0, ext: 6029.1, change: 3, osv: 2},
-    {index: 992, price: 6755.45, trend: 1, hi: 6755.45, lo: 6439.96, up: 0, down: 0, ext: 6439.96, change: 1, osv: 0}
+const expected = [
+    { index: 60, price: 5586.61, trend: -1, hi: 5825.01, lo: 5586.61, up: 0, down: 0, ext: 5825.01, change: -1, osv: 0 },
+    { index: 92, price: 5217.82, trend: 1, hi: 5217.82, lo: 5028.15, up: 0, down: 0, ext: 5028.15, change: 1, osv: 0 },
+    { index: 192, price: 5748.97, trend: 1, hi: 5757.86, lo: 5109.4, up: 0, down: 0, ext: 5109.4, change: 4, osv: 3 },
+    { index: 292, price: 5904.49, trend: 1, hi: 5904.49, lo: 5598.23, up: 0, down: 0, ext: 5598.23, change: 1, osv: 0 },
+    { index: 392, price: 5095.3, trend: -1, hi: 5357.63, lo: 5040.76, up: 0, down: 0, ext: 5357.63, change: -1, osv: 0 },
+    { index: 492, price: 5636.64, trend: 1, hi: 5699.91, lo: 5364.99, up: 0, down: 0, ext: 5364.99, change: 1, osv: 0 },
+    { index: 592, price: 5435.08, trend: 1, hi: 5447.79, lo: 5260.19, up: 0, down: 0, ext: 5260.19, change: 1, osv: 0 },
+    { index: 692, price: 5795.1, trend: 1, hi: 5917.05, lo: 5657.86, up: 0, down: 0, ext: 5657.86, change: 0, osv: 0 },
+    { index: 792, price: 6388.55, trend: 1, hi: 6529.41, lo: 5605.59, up: 0, down: 0, ext: 5605.59, change: 4, osv: 3 },
+    { index: 892, price: 6587.43, trend: 1, hi: 6681.98, lo: 6029.1, up: 0, down: 0, ext: 6029.1, change: 3, osv: 2 },
+    { index: 992, price: 6755.45, trend: 1, hi: 6755.45, lo: 6439.96, up: 0, down: 0, ext: 6439.96, change: 1, osv: 0 }
 ];
 
-test("ftse100", function (t) {
-    var threshold = 0.03,
+test("ftse100", t => {
+    const threshold = 0.03,
         trend = [],
         hi = [],
         lo = [],
@@ -31,8 +31,8 @@ test("ftse100", function (t) {
 
     t.plan(99);
 
-    series.forEach(function (price) {
-        var res = strategy.directionalChange("ftse100", price, threshold);
+    series.forEach(price => {
+        const res = strategy.directionalChange("ftse100", price, threshold);
 
         trend.push(res.trend);
         hi.push(res.hi);
@@ -44,7 +44,7 @@ test("ftse100", function (t) {
         osv.push(res.osv);
     });
 
-    expected.forEach(function (value) {
+    expected.forEach(value => {
         t.equal(value.price, series[value.index], "value");
         t.equal(value.trend, trend[value.index], "trend");
         t.equal(value.hi, hi[value.index], "hi");
